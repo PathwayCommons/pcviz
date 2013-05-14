1c543ab220a9f896615ba203c5cddc6feffc2646
B. Arman Aksoy <arman@aksoy.org>
2013-05-14 18:00:23 -0400

    new gene service in place -- but not working properly yet.

---
+++ src/test/java/org/pathwaycommons/pcviz/service/GeneNameServiceTest.java
@@ -0,0 +1,21 @@
+package org.pathwaycommons.pcviz.service;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static junit.framework.Assert.assertEquals;
+
+public class GeneNameServiceTest {
+    protected ClassPathXmlApplicationContext context
+            = new ClassPathXmlApplicationContext("classpath*:META-INF/spring/testContext.xml");
+
+    @Test
+    public void testValidation() {
+        GeneNameService geneNameService = (GeneNameService) context.getBean("geneNameService");
+        assertEquals(1, geneNameService.validate("A2M").getMatches().size());
+        assertEquals(1, geneNameService.validate("AADAC").getMatches().size());
+        assertEquals(1, geneNameService.validate("CES5A1").getMatches().size());
+        assertEquals(1, geneNameService.validate("SNAT").getMatches().size());
+        assertEquals("AANAT", geneNameService.validate("SNAT").getMatches().iterator().next());
+    }
+}
