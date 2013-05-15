a89aa75f9a0039f797446d08a0a35b940b9319a5
B. Arman Aksoy <arman@aksoy.org>
2013-05-15 10:29:46 -0400

    barebones autocomplete function started working. Still need some UI customization.

--- src/main/java/org/pathwaycommons/pcviz/service/GeneNameService.java
+++ src/main/java/org/pathwaycommons/pcviz/service/GeneNameService.java
@@ -2,19 +2,19 @@ package org.pathwaycommons.pcviz.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.pathwaycommons.pcviz.model.AutoCompleteResult;
 import org.pathwaycommons.pcviz.model.GeneValidation;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class GeneNameService {
     private static Log log = LogFactory.getLog(GeneNameService.class);
 
     private Resource geneResource;
     private HashMap<String, HashSet<String>> geneMaps = null;
+    private Integer autoCompleteLimit = Integer.MAX_VALUE;
 
     public Resource getGeneResource() {
         return geneResource;
@@ -24,8 +24,16 @@ public class GeneNameService {
         this.geneResource = geneResource;
     }
 
+    public Integer getAutoCompleteLimit() {
+        return autoCompleteLimit;
+    }
+
+    public void setAutoCompleteLimit(Integer autoCompleteLimit) {
+        this.autoCompleteLimit = autoCompleteLimit;
+    }
+
     public GeneValidation validate(String name) {
-        if(geneMaps == null) initializeNameMap();
+        initializeNameMap();
 
         GeneValidation geneValidation = new GeneValidation();
         geneValidation.setQuery(name);
@@ -36,6 +44,8 @@ public class GeneNameService {
     }
 
     private void initializeNameMap() {
+        if(geneMaps != null) return;
+
         try {
             geneMaps = new HashMap<String, HashSet<String>>();
             Scanner scanner = new Scanner(getGeneResource().getInputStream());
@@ -71,5 +81,37 @@ public class GeneNameService {
         }
 
         strings.add(primaryName);
+    }
+
+    public List<AutoCompleteResult> autoComplete(String term) {
+        initializeNameMap();
+
+        List<AutoCompleteResult> autoCompleteResults = new ArrayList<AutoCompleteResult>();
+
+        term = term.toUpperCase();
+
+        for (String key : geneMaps.keySet()) {
+            if(key.startsWith(term)) {
+                HashSet<String> matches = geneMaps.get(key);
+                assert matches != null;
+
+                for (String match : matches) {
+                    AutoCompleteResult autoCompleteResult = new AutoCompleteResult();
+                    autoCompleteResult.setId(match);
+                    String label = key.equalsIgnoreCase(match)
+                            ? key
+                            : match + " (" + key + ")";
+                    autoCompleteResult.setLabel(label);
+                    autoCompleteResult.setValue(match);
+                    autoCompleteResults.add(autoCompleteResult);
+                }
+
+                if(autoCompleteResults.size() >= getAutoCompleteLimit())
+                    break;
+            }
+
+        }
+
+        return autoCompleteResults;
     }
 }
