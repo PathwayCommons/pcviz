dc7203f30d5f8c6383219474ccb1778c1cdbf150
B. Arman Aksoy <arman@aksoy.org>
2013-05-28 00:52:11 -0400

    minor fix to importance values for too-far-away-nodes.

--- src/main/webapp/js/components/pcviz.ranker.js
+++ src/main/webapp/js/components/pcviz.ranker.js
@@ -1,4 +1,5 @@
 var PCVizRanker = function(cy) {
+    cy.$("node").data("importance", 2);
     cy.$("node[?isseed]").bfs(function(i, depth) {
             var node = this;
             node.data("importance", 4-depth);
