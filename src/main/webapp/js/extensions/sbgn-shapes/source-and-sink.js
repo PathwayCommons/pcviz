f5f83de70836cc2e2cebb5b82e0adb4c8e94bc4f
mecitsari <mecitsari@localhost>
2014-02-26 14:26:20 +0200

    misssing js files are added

---
+++ src/main/webapp/js/extensions/sbgn-shapes/source-and-sink.js
@@ -0,0 +1,97 @@
+;(function($$){"use strict";
+
+	var CanvasRenderer = $$('renderer', 'canvas');
+	var renderer = CanvasRenderer.prototype;
+	
+	//default node shapes are in nodeShape array,
+	//all different types must be added
+	var nodeShape = $$.style.types.nodeShape.enums;
+	nodeShape.push("source and sink");
+
+	var nodeShapes = CanvasRenderer.nodeShapes;
+
+	nodeShapes["source and sink"] = {
+		points: $$.math.generateUnitNgonPoints(4, 0),
+
+		draw: function(context, node) {
+			nodeShapes["source and sink"].drawPath(context, node);
+			context.fill();
+
+		},
+
+		drawPath: function(context, node) {
+			var centerX = node._private.position.x;
+			var centerY = node._private.position.y;;
+			var width = node.width();
+			var height = node.height();
+			var label = node._private.data.sbgnlabel;
+			var pts = nodeShapes["source and sink"].points;
+
+			nodeShapes["ellipse"].drawPath(context, centerX, centerY,
+				width, height);
+
+			context.stroke();
+
+			context.beginPath();
+			context.translate(centerX, centerY);
+			context.scale(width * Math.sqrt(2) / 2, height * Math.sqrt(2) / 2);
+
+			context.moveTo(pts[2], pts[3]);
+			context.lineTo(pts[6], pts[7]);
+			context.closePath();
+
+			context.scale(2/(width * Math.sqrt(2)), 2/(height * Math.sqrt(2)));
+			context.translate(-centerX, -centerY);	
+
+		},
+
+		intersectLine: function(node, x, y) {
+			var centerX = node._private.position.x;
+			var centerY = node._private.position.y;
+			var width = node.width();
+			var height = node.height();
+			var padding = node._private.style["border-width"].value / 2;
+
+			return nodeShapes["ellipse"].intersectLine(centerX, centerY, width, 
+				height, x, y, padding);
+
+		},
+
+		intersectBox: function(x1, y1, x2, y2, node) {
+			var centerX = node._private.position.x;
+			var centerY = node._private.position.y;
+			var width = node.width();
+			var height = node.height();
+			var padding = node._private.style["border-width"].value / 2;
+
+			return nodeShapes["ellipse"].intersectBox(x1, y1, x2, y2, width, height, 
+				centerX, centerY, padding);
+
+		},
+
+		checkPointRough: function(x, y, node, threshold) {
+			var centerX = node._private.position.x;
+			var centerY = node._private.position.y;
+			var width = node.width() + threshold;
+			var height = node.height() + threshold;
+			var padding = node._private.style["border-width"].value / 2;
+
+			return nodeShapes["ellipse"].checkPointRough(x, y, padding, width, height, 
+				centerX, centerY);
+
+		},
+
+		checkPoint: function(x, y, node, threshold) {
+			var centerX = node._private.position.x;
+			var centerY = node._private.position.y;
+			var width = node.width();
+			var height = node.height();
+			var padding = node._private.style["border-width"].value / 2;
+
+			return nodeShapes["ellipse"].checkPoint(x, y, padding, width, 
+				height, centerX, centerY)
+
+		}
+	}
+
+})( cytoscape );
