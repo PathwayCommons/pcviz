16b513cf7224580c4c0a14cc5e6569236080cf35
mecitsari <mecitsari@localhost>
2014-03-17 14:26:06 +0200

    missing files added

---
+++ src/main/webapp/js/extensions/sbgn-shapes/arc-shapes/production.js
@@ -0,0 +1,97 @@
+;(function($$){"use strict";
+	var CanvasRenderer = $$('renderer', 'canvas');
+	var renderer = CanvasRenderer.prototype;
+	
+	//default node shapes are in nodeShape array,
+	//all different types must be added
+	var arrowShape = $$.style.types.arrowShape.enums;
+	arrowShape.push("production");
+	//sbgnShapes["necessary-stimulation"] = true;
+
+	var arrowShapes = CanvasRenderer.arrowShapes;
+
+	var bbCollide = function(x, y, centerX, centerY, width, height, direction, padding){
+    	var x1 = centerX - width/2;
+    	var x2 = centerX + width/2;
+    	var y1 = centerY - height/2;
+    	var y2 = centerY + height/2;
+
+    	return (x1 <= x && x <= x2) && (y1 <= y && y <= y2);
+  	};
+
+	arrowShapes["production"] = {
+	    points: [
+	      0, -0.40,
+	      0.30, -0.40,
+	      0.30, -0.70,
+	      0, -0.70
+	    ],
+
+	   	textPoints: [
+	    	0.20, -0.50,
+	    	0.10, -0.50,
+	    	0.20, -0.60,
+	    	0.10, -0.60
+	    ],
+
+	   	trianglePoints: [
+    		-0.15, -0.3,
+      		0, 0,
+      		0.15, -0.3
+    	],
+
+	    collide: function(x, y, centerX, centerY, width, height, direction, padding) {
+	      var points = arrowShapes["production"].trianglePoints;
+	      	      
+	      return $$.math.pointInsidePolygon(
+	        x, y, points, centerX, centerY, width, height, direction, padding);
+	    },
+	    
+	    roughCollide: bbCollide,
+	    
+	    draw: function(context) {
+	    	var points = arrowShapes["production"].points;
+	    	var trianglePoints = arrowShapes["production"].trianglePoints;
+	    	var textPoints = arrowShapes["production"].textPoints;
+
+	    	//triangle
+	    	for (var i = 0; i < trianglePoints.length / 2 + 1 ; i++) {
+	        	context.lineTo(trianglePoints[(i * 2)% trianglePoints.length], 
+	        		trianglePoints[(i * 2 + 1)% trianglePoints.length]);
+	      	}
+
+			var oldColor  = context.fillStyle;
+			context.fillStyle = "#000000";
+			context.fill();
+			context.fillStyle = oldColor;
+
+	      	context.moveTo(points[0], points[1]);
+
+	      	//square
+	      	context.beginPath();
+	    	for (var i = 0; i < points.length / 2 ; i++) {
+	        	context.lineTo(points[(i * 2)], 
+	        		points[(i * 2 + 1)]);
+	      	}
+	      	context.closePath();
+
+	      	//text
+			context.lineJoin="round";
+	      	context.moveTo(textPoints[0], textPoints[1]);
+	      	for (var i = 0; i < textPoints.length / 2 ; i++) {
+	        	context.lineTo(textPoints[(i * 2)], textPoints[(i * 2 + 1)]);
+	      	}
+	      	context.moveTo(textPoints[6], textPoints[7]);
+
+	    },
+	    
+	    spacing: function(edge) {
+	      return 0;
+	    },
+	    
+	    gap: function(edge) {
+	      return 0;
+	    }
+	}
+
+})( cytoscape );
