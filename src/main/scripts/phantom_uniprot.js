b6811ff517847fd7d9c90cdb35b46b428579aa9a
Onur Sumer <s.onur.sumer@gmail.com>
2014-07-25 19:48:30 -0400

    added phantom script for automated uniprot id layout and sshot

---
+++ src/main/scripts/phantom_uniprot.js
@@ -0,0 +1,197 @@
+var webPage = require('webpage');
+var fs = require('fs');
+var system = require('system');
+
+function main(args)
+{
+	if (args.length < 3)
+	{
+		console.log("Usage: cache_layout.js <input_hgnc_file> <output_directory>");
+		phantom.exit(1);
+	}
+	else
+	{
+		var inFile = args[1];
+		var outDir = args[2];
+
+		var inputStream = fs.open(inFile, 'r');
+		var buffer = [];
+
+		// skip first line
+		inputStream.readLine();
+
+		// fetch all for the given inputStream
+		fetch(inputStream, buffer, outDir);
+
+		// TODO find a way to properly phantom.exit();
+	}
+}
+
+/**
+ * Fetches the web page content for the next uniprot id.
+ *
+ *
+ * @param inputStream   input stream containing uniprot ids
+ * @param buffer        buffer that may contain uniprot ids
+ * @param outDir        target output directory
+ * @returns String  next uniprot id
+ */
+function fetch(inputStream, buffer, outDir)
+{
+	var uniprotId = getNextUniprotId(inputStream, buffer);
+
+	if (uniprotId)
+	{
+		var page = webPage.create();
+
+		// recursion...
+		chainedFetch(page, inputStream, buffer, outDir);
+
+		// fetch n-hood and screenshot from the web page
+		fetchNhood(page, uniprotId, outDir);
+	}
+
+	return uniprotId;
+}
+
+function chainedFetch(page, inputStream, buffer, outDir)
+{
+	page.onClosing = function(closingPage) {
+		// recursion...
+		fetch(inputStream, buffer, outDir);
+	};
+}
+
+/**
+ * Extracts the next valid uniprot id from the given input stream
+ *
+ * @param inputStream   source input stream
+ * @param buffer        buffer for previous fetch
+ * @returns {*}
+ */
+function getNextUniprotId(inputStream, buffer)
+{
+	// (one line may contain more than one uniprot id)
+	if (buffer.length > 0)
+	{
+		return buffer.pop();
+	}
+
+	// nothing to read...
+	if (inputStream.atEnd())
+	{
+		return null;
+	}
+
+	var line = inputStream.readLine();
+	var parts = line.split("\t");
+
+	if (parts.length > 2)
+	{
+		// get uniprot ids
+		var uniprots = parts[2].split(",");
+
+		// fetch n-hood for each uniprot id
+		for (var i=0; i<uniprots.length; i++)
+		{
+			var uniprotId = uniprots[i].trim();
+
+			if (uniprotId.length > 0)
+			{
+				buffer.push(uniprotId);
+			}
+		}
+	}
+
+	if (buffer.length > 0)
+	{
+		return buffer.pop();
+	}
+	else
+	{
+		// tail recursion...
+		return getNextUniprotId(inputStream, buffer);
+	}
+}
+
+/**
+ * Fetches the result of n-hood query for the given input identifier (gene or uniprot id).
+ * Writes the result as a JSON string into the output file.
+ *
+ * @param page
+ * @param queryString identifier (gene symbol or uniprot id)
+ * @param outputDir directory to save output files
+ */
+function fetchNhood(page, queryString, outputDir)
+{
+	console.log("fetchNHood(): " + queryString);
+
+	page.open('http://www.pathwaycommons.org/pcviz/#neighborhood/' + queryString, function() {
+		var same = 0;
+		var prevMsg = "";
+
+		var interval = setInterval(function() {
+			page.evaluate(function() {
+				if (cy)
+				{
+					// TODO see if there is way to check the layout is finished!
+					console.log("NODE_POSITION__" + JSON.stringify(cy.elements("node").position()));
+				}
+			});
+		}, 200);
+
+		function saveGraph()
+		{
+			// get graph json for the current uniprot id
+			var graphJson = page.evaluate(function() {
+				if (cy)
+				{
+					return JSON.stringify(window.cy.elements().jsons());
+				}
+
+				return null;
+			});
+
+			if (graphJson)
+			{
+				page.render(outputDir + "/" + queryString + ".png");
+
+				try {
+					fs.write(outputDir + "/" + queryString + ".json", graphJson, 'w');
+				} catch(e) {
+					console.log(e);
+				}
+			}
+		}
+
+		page.onConsoleMessage = function(msg) {
+			// TODO this is really a bad of checking layout end
+			if (msg.split("__")[0] == "NODE_POSITION")
+			{
+				// checking if node position changed within the interval
+				if (msg == prevMsg)
+				{
+					same++;
+				}
+				else
+				{
+					prevMsg = msg;
+					same = 0;
+				}
+
+				// if no change for more than 3 successive intervals,
+				// assuming that layout is finished...
+				if (same > 3)
+				{
+					clearInterval(interval);
+					saveGraph();
+					page.close();
+				}
+			}
+
+			console.log(msg);
+		};
+	});
+}
+
+main(system.args);
