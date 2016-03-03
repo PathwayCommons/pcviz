06bec1b4584e0717ea53eecdab4b26f54c5e4ad3
IgorRodchenkov <rodche@gmail.com>
2016-03-03 12:54:05 -0500

    Refs #21 - modified the script to be able to re-run and continue processing new (or previously skipped) uniprot IDs from the list.

--- src/main/scripts/uniprot_scraper.sh
+++ src/main/scripts/uniprot_scraper.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
-
-#TODO sanitize args
+# TODO why don't we simplify the scripts to use stdin/stdout and then chain them via shell pipe and i/o redirect (|, >, <, >>, etc.)?
+# TODO sanitize args
 # args:
 #   $1 : input hgnc file
 #   $2 : intermediate file (to output uniprot ids)
 #   $3 : output directory
 #   $4 : logfile name
 
-echo "creating a list of UniProt IDs..."
-# extract uniprot ids into a temp file
-phantomjs extract_uniprot.js $1 $2
+if ! [ -e "$2" ]; then
+    echo "creating a list of UniProt IDs..."
+    # extract uniprot ids into a temp file
+    phantomjs extract_uniprot.js $1 $2
+    wait
+fi
 
-wait
 echo "for each ID, querying pcviz/PC2 for its nearest undirected neighborhood network:"
 tempFile="$2";
 
-# process the temp file and scrape for each uniprot id
+# process the temp file and scrape for each (not previously processed) uniprot accession number
 numOfIds=`wc -l ${tempFile} | awk '{ print $1 }'`
 echo "there are $numOfIds to process;"
-
 i=1
 while [ $i -le $numOfIds ]
 do
     uniprotId=`head -n $i ${tempFile}| tail -1`
-    phantomjs uniprot_scraper.js $uniprotId $3 >> $4
+
+    if [ -e "$3/$uniprotId.json" ]; then
+      echo "skip existing $uniprotId"
+    else
+      phantomjs uniprot_scraper.js $uniprotId $3 >> $4
+    fi
+
     echo "processed: $i"
     let i=$i+1
 done
 
 rm $2;
 echo "all done!"
-
